77587e57a08e4d81d2e4d5e2c97624327c5f881b
Christopher Bonner <cbonner.dev@outlook.com>
2024-02-19 01:48:46 +0000

    Added categories linked list

--- main.c
+++ main.c
@@ -7,106 +7,237 @@
 #define MAX_DESCRIPTION_LENGTH 200
 #define MAX_CATEGORY_LENGTH 100
 
+// A datatype for categories
+typedef struct Category
+{
+    char name[MAX_CATEGORY_LENGTH];
+    struct Category *p_next;
+} Category;
+
 // A datatype for keybinds
 typedef struct Keybind
 {
     char bind[MAX_BIND_LENGTH];
     char description[MAX_DESCRIPTION_LENGTH];
-    char category[MAX_CATEGORY_LENGTH];
+    struct Category *p_category;
     struct Keybind *p_next;
 } Keybind;
 
+
 // Creates the new bind
-Keybind *create_bind(char *p_bind, char *p_description, char *p_category)
+Keybind *create_bind(char *p_bind, char *p_description, Category *p_category)
 {
     // Allocates memory and returns a bind
     Keybind *newBind = (Keybind *)malloc(sizeof(Keybind));
     strncpy(newBind->bind, p_bind, MAX_BIND_LENGTH);
     strncpy(newBind->description, p_description, MAX_DESCRIPTION_LENGTH);
-    strncpy(newBind->category, p_category, MAX_CATEGORY_LENGTH);
+    newBind->p_category = p_category;
     newBind->p_next = NULL;
 
     return newBind;
 }
 
-// Adds the binds into a linked list
-void add_next_bind(Keybind *p_keybind, Keybind *p_nextBind)
+// Creates the category
+Category *create_category(char *p_name)
+{
+    // Allocates memory and returns a category
+    Category *newCategory = (Category *)malloc(sizeof(Category));
+    strncpy(newCategory->name, p_name, MAX_CATEGORY_LENGTH);
+    newCategory->p_next = NULL;
+
+    return newCategory;
+}
+
+// Appends the binds into a linked list
+void append_bind(Keybind *head, Keybind *p_keybind)
 {
     // Checks to see whether the binds are null
-    if (p_keybind == NULL || p_nextBind == NULL)
+    if (head == NULL || p_keybind == NULL)
     {
         printf("\n Task allocation failed");
     }
     else
     {
         // Traverses linked list until there are no more elements and adds it to the end
-        Keybind *p_current = p_keybind;
+        Keybind *p_current = head;
         while (p_current->p_next != NULL)
         {
             p_current = p_current->p_next;
         }
-        p_current->p_next = p_nextBind;
+        p_current->p_next = p_keybind;
     }
 }
 
-// Displays a linked list
-void display_list(Keybind *p_bind)
+// Appends the categories into a linked list
+void append_category(Category *head, Category *p_category)
 {
-    Keybind *p_currentBind = p_bind;
+    // Checks to see whether the binds are null
+    if (head == NULL || p_category == NULL)
+    {
+        printf("\n Task allocation failed");
+    }
+    else
+    {
+        // Traverses linked list until there are no more elements and adds it to the end
+        Keybind *p_current = head;
+        while (p_current->p_next != NULL)
+        {
+            p_current = p_current->p_next;
+        }
+        p_current->p_next = p_category;
+    }
+}
+
+
+// Prepends the binds into the keybinds linked list
+void prepend_bind(Keybind *head, Keybind *p_keybind)
+{
+    if (head == NULL || p_keybind == NULL)
+    {
+        printf("\n Prepending keybind failed");
+    }
+    else
+    {
+        // Checks to see if there are elements within the linked list
+        if (head->p_next != NULL)
+        {
+            // Sets the keybind to be the next after head by swapping its pointers
+            p_keybind->p_next = head->p_next;
+            head->p_next = p_keybind;
+        }
+        else
+        {
+            head->p_next = p_keybind;
+        }
+    }
+}
+
+// Prepends the category into the categories linked list
+void prepend_category(Category *head, Category *p_category)
+{
+    if (head == NULL || p_category == NULL)
+    {
+        printf("\n Prepending category failed");
+    }
+    else
+    {
+        // Checks to see if there are elements within the linked list
+        if (head->p_next != NULL)
+        {
+            // Sets the category to be the next after head by swapping its pointers
+            p_category->p_next = head->p_next;
+            head->p_next = p_category;
+        }
+        else
+        {
+            head->p_next = p_category;
+        }
+    }
+}
+
+
+// Displays the keybinds list
+void display_keybinds(Keybind *head)
+{
+    Keybind *p_currentBind = head;
     
     // Traverses linked list and prints to the console the binds' bind, description, and category
     int index = 0;
     while (p_currentBind != NULL)
     {
         index++;
-        printf("%d: %s - %s - %s\n", index, p_currentBind->bind, p_currentBind->description, p_currentBind->category);
+        printf("%d: %s - %s - %s\n", index, p_currentBind->bind, p_currentBind->description, p_currentBind->p_category->name);
         p_currentBind = p_currentBind->p_next;
     }
 }
 
+// Displays the categories list
+void display_categories(Category *head)
+{
+    Category *p_currentCategory = head;
+    
+    // Traverses linked list and prints to the console the category's name
+    int index = 0;
+    while (p_currentCategory != NULL)
+    {
+        index++;
+        printf("%d: %s\n", index, p_currentCategory->name);
+        p_currentCategory = p_currentCategory->p_next;
+    }
+}
+
 // Recursively frees all the memory within the linked list
-void free_list(Keybind *p_bind)
+void free_keybind_list(Keybind *head)
 {
     // Checks to see if the bind is null
-    if (p_bind == NULL)
+    if (head == NULL)
         return;
     
     // Reruns the function for the next element in the list
-    free_list(p_bind->p_next);
+    free_keybind_list(head->p_next);
 
     // Frees the current bind
-    free(p_bind);
+    free(head);
+}
+
+// Recursively frees all the memory within the linked list
+void free_category_list(Category *head)
+{
+    // Checks to see if the category is null
+    if (head == NULL)
+        return;
+    
+    // Reruns the function for the next element in the list
+    free_category_list(head->p_next);
+
+    // Frees the current category
+    free(head);
+
+}
+
+// Initialises the head of the keybinds linked list
+Keybind *init_keybind_head()
+{
+    Keybind *head = (Keybind *)malloc(sizeof(Keybind));
+    strncpy(head->bind, "", MAX_BIND_LENGTH);
+    strncpy(head->description, "", MAX_DESCRIPTION_LENGTH);
+    head->p_category = NULL;
+    head->p_next = NULL;
+
+    return head;
+}
+
+// Initialises the head of the categories linked list
+Category *init_category_head()
+{
+    Category *head = (Category *)malloc(sizeof(Category));
+    strncpy(head->name, "", MAX_CATEGORY_LENGTH);
+    head->p_next = NULL;
+
+    return head;
+}
+
+void input_bind(Keybind *head)
+{
+    char bind[MAX_BIND_LENGTH];
+    char description[MAX_DESCRIPTION_LENGTH];
+    char category[MAX_CATEGORY_LENGTH];
+
+    printf("What is the keybind? ");
 }
 
 // Main function
 int main()
 {
-    // Some test binds
-    Keybind *test1 = create_bind("Test 1", "Test Description 1", "Test Category");
-    Keybind *test2 = create_bind("Test 2", "Test Description 2", "Test Category");
-    Keybind *test3 = create_bind("Test 3", "Test Description 3", "Test Category");
-    Keybind *test4 = create_bind("Test 4", "Test Description 4", "Test Category");
-    Keybind *test5 = create_bind("Test 5", "Test Description 5", "Test Category");
-    Keybind *test6 = create_bind("Test 6", "Test Description 6", "Test Category");
-    Keybind *test7 = create_bind("Test 7", "Test Description 7", "Test Category");
-    Keybind *test8 = create_bind("Test 8", "Test Description 8", "Test Category");
-    Keybind *test9 = create_bind("Test 9", "Test Description 9", "Test Category");
-    
-    // Add binds to linked list
-    add_next_bind(test1, test2);
-    add_next_bind(test1, test3);
-    add_next_bind(test1, test4);
-    add_next_bind(test1, test5);
-    add_next_bind(test1, test6);
-    add_next_bind(test1, test7);
-    add_next_bind(test1, test8);
-    add_next_bind(test1, test9);
-    
-    // Displays the binds
-    display_list(test1);
-    
+    // Create head nodes
+    Keybind *keybindHead = init_keybind_head();
+    Category *categoryHead = init_category_head();
+
     // Frees all the memory used up by the keybinds linked list
-    free_list(test1);
+    free_keybind_list(keybindHead);
+    
+    // Frees all the memory used up by the categories linked list
+    free_category_list(categoryHead);
 
     return 0;
 }
